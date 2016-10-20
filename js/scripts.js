0d44d738ed91f984993654dda86bbaac347c18a4
Michael Munyaneza and Michael Poehner <michael@>
2016-10-19 20:17:39 -0700

    show that branching is working properly and different results are shown for different checkboxes checked

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
   $("form#fortune").submit(function(event){
     var fortune1 = $("input:checkbox[name=events]:checked").val();
 
-        if (fortune1 === 'cat' && 'mirror' || 'umbrella')  {
+        if (fortune1 === 'cat' && 'mirror' && 'umbrella')  {
           $("#cat-mirror-umbrella").show();
         }
-        else if (fortune1 === 'Europe'){
-          $("#Europe_text").show();
+        else if (fortune1 === 'shadows' && 'clowns' && 'murder') {
+          $("#shadows-clowns-murder").show();
         }
-        else if (fortune1 === 'America'){
-          $("#America_text").show();
+        else if (fortune1 === 'cannibal' && 'medium' && 'dreams'){
+          $("#cannibal-medium-dreams").show();
         }
-    $("#fortune-responses").show();
+        else {
+          $("#story").show();
+        }
+    // $("#fortune-responses").show();
     $("input:checkbox[name=events]:checked").each(function(){
       var fortuneCalculator = $(this).val();
       // $('#fortune-responses').append(fortuneCalculator + "<br>");
